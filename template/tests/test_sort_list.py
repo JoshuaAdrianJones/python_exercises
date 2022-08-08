05c24046408bf90277ab0dde943978c772ebb9bb
Joshua Jones <joshua.ar.jones@gmail.com>
2022-08-08 23:45:49 +0100

    [cleanup] fix test file imports

--- template/tests/test_sort_list.py
+++ template/tests/test_sort_list.py
@@ -1,21 +1,22 @@
+"""
+tests for sort list module
+"""
 import unittest
-import sys
-from unittest import result # added!
-sys.path.append("..") # added!
-
 from src.sort_list import sort_list
 
-class TestSortListMethods(unittest.TestCase):
 
-    def testSortList(self):
+class TestSortList(unittest.TestCase):
+    """test suite for sort list"""
+
+    def test_sort_list(self):
         """
         Test if sortList can sort a list of integers
         """
-        data = [2,6,4]
+        data = [2, 6, 4]
         result = sort_list(data)
-        self.assertEqual(result, [2,4,6])
+        self.assertEqual(result, [2, 4, 6])
 
-    def testReturnSame(self):
+    def test_return_same(self):
         """
         If length = 1 return the same item.
         """
@@ -23,19 +24,23 @@ class TestSortListMethods(unittest.TestCase):
         result = sort_list(data)
         self.assertEqual(result, data)
 
-    def testSortImmutable(self):
+    def test_sort_immutable(self):
         """
         Test that the original list remains unmodified
         """
         data = [23, 6, 9, 4]
         result = sort_list(data)
-        self.assertEqual(result, [4,6,9,23])
-        self.assertEqual(data, [23,6,9,4])
-    
-    def testNegative(self):
+        self.assertEqual(result, [4, 6, 9, 23])
+        self.assertEqual(data, [23, 6, 9, 4])
+
+    def test_negative(self):
+        """
+        test function can handle negative numbers correctly
+        """
         data = [-23, 6, -9, 40000]
         result = sort_list(data)
-        self.assertEqual(result, [-23,-9, 6, 40000])
+        self.assertEqual(result, [-23, -9, 6, 40000])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
