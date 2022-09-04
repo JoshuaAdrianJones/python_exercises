904d674080d80d2ef2d93c38c48efd6e5b89aecf
Joshua Jones <joshua.ar.jones@gmail.com>
2022-09-04 21:45:57 +0100

    add exercise 3

---
+++ template/src/tests/test_starter.py
@@ -0,0 +1,17 @@
+"""
+    Test suite
+"""
+
+import unittest
+
+
+class TestStarter(unittest.TestCase):
+    """
+    Test suite
+    """
+
+    def test_template(self) -> None:
+        """
+        Test function
+        """
+        assert 1 + 1 == 2
